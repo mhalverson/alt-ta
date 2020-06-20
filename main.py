3a462d3655c16b4695afad196857c6d6679ba696
Matt Halverson <mhalverson@gmail.com>
2020-06-20 20:47:26 +1200

    add routeline connecting the dots

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if not LINZ_API_KEY:
     raise ValueError('no LINZ_API_KEY found at {}'.format(LINZ_API_KEY_PATH))
 
 topo = folium.TileLayer(
-    name='NZ Topo 50',
+    name='NZ Topo50',
     tiles='http://tiles-{s}.data-cdn.linz.govt.nz/services;key=' + LINZ_API_KEY + '/tiles/v4/layer=50767/EPSG:3857/{z}/{x}/{y}.png',
     attr='<a href=“http://data.linz.govt.nz”>Sourced from LINZ. CC BY 4.0</a>',
     subdomains='abcd',
@@ -35,29 +35,68 @@ m.add_child(folium.LatLngPopup()) # enable this if you want to click to see the 
 
 KEY_SHORT_DESC = 'short_desc'
 KEY_LOCATION = 'location'
+KEY_WAYPOINTS = 'waypoints'
 
-campsites = [
-    {
-        KEY_SHORT_DESC: 'Separation Point',
-        KEY_LOCATION: latlng_separation_point,
-    },
-    {
-        KEY_SHORT_DESC: 'Awapoto Hut',
-        KEY_LOCATION: (-40.8627, 172.9388),
-    },
+route = [
+    { KEY_SHORT_DESC: 'Road end', KEY_LOCATION: (-40.8098, 172.9563), },
+    { KEY_SHORT_DESC: 'Whariwharangi Hut', KEY_LOCATION: (-40.7890, 172.9744), },
+    { KEY_SHORT_DESC: 'Separation Point', KEY_LOCATION: latlng_separation_point, },
+    { KEY_WAYPOINTS: ((-40.8043, 173.0060), (-40.8165, 172.9772), (-40.8328, 172.9683),),
+      KEY_SHORT_DESC: 'Awapoto Hut', KEY_LOCATION: (-40.8627, 172.9388), },
+    { KEY_SHORT_DESC: 'Wainui Hut', KEY_LOCATION: (-40.9147, 172.9203), },
+    { KEY_WAYPOINTS: ((-40.9377, 172.9003), (-40.8970, 172.7300),),
+      KEY_SHORT_DESC: 'Anatoki Forks Hut', KEY_LOCATION: (-40.9396, 172.6084), },
+    { KEY_SHORT_DESC: 'Adelaide Tarn', KEY_LOCATION: (-40.9430, 172.5435), },
+    { KEY_SHORT_DESC: 'Lonely Lake', KEY_LOCATION: (-40.9828, 172.5580), },
+    { KEY_WAYPOINTS: ((-41.0357, 172.5494),),
+      KEY_SHORT_DESC: 'Fenella', KEY_LOCATION: (-41.04960, 172.52520), },
 ]
 
 # put it all together
 
-for x in campsites:
+# Camp
+fg_camp_name = 'Campsites'
+fg_camp = folium.FeatureGroup(name=fg_camp_name, show=True)
+fg_camp.add_to(m)
+
+for x in route:
     popup = folium.Popup(x[KEY_SHORT_DESC])
     marker = folium.Marker(
         location=x[KEY_LOCATION],
         popup=popup,
     )
-    marker.add_to(m)
+    marker.add_to(fg_camp)
 
+# Route
+fg_route = folium.FeatureGroup(name='Route', show=True)
+fg_route.add_to(m)
 
+for i, day in enumerate(route):
+    leg = []
+    if i == 0:
+        pass # do nothing!
+    else:
+        wake_up_coord = route[i-1][KEY_LOCATION]
+        leg.append(wake_up_coord)
+    if day.get(KEY_WAYPOINTS):
+        leg.extend(day[KEY_WAYPOINTS])
+    sleep_coord = day[KEY_LOCATION]
+    leg.append(sleep_coord)
+
+    if len(leg) < 2:
+        continue
+
+    folium.PolyLine(
+        locations=leg,
+        color='#FF0000',
+        weight=5,
+        #popup='{}'.format(day[DAY_DATE]),
+    ).add_to(fg_route)
+
+# LayerControl - this has to come after all the FeatureGroups
+folium.LayerControl().add_to(m)
+
+# Write to file and call it a day
 map_filename = os.path.join(HTML_DIR, 'map.html')
 print('Writing map HTML to file: {}'.format(map_filename))
 with open(map_filename, 'w'):
