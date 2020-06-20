6859c81d2992702fee2e629fb1d1d801211da086
Matt Halverson <mhalverson@gmail.com>
2020-06-21 11:08:26 +1200

    first attempt at Kahurangi section
    Have completed my first attempt at the Kahurangi section... beginning
    where Kirstie had suggested, ending at Murchison (Matiri River).
    
    There are two alternate options I thought made sense - could start at
    Farewell Spit in the north, and could end at Lyell (Old Ghost Road) in
    the south.
    
    References for tricky bits:
    
    1) (Proposed) Stone to Hurricane -  https://www.doc.govt.nz/parks-and-recreation/places-to-go/nelson-tasman/places/kahurangi-national-park/things-to-do/tracks/matiri-valley-and-1000-acre-plateau-tramping-tracks/
    
    2) (Alternate - Farewell Spit) Boulder Lake to Adelaide - https://www.wildthings.club/trails/tasman/takaka/boulder-lake-to-anatoki-forks/?
    
    3) (Alternate - Old Ghost Road) Johnson Hut to OGR - http://remotehuts.co.nz/huts/Johnson_Hut/

--- main.py
+++ main.py
@@ -34,13 +34,17 @@ for topo in topos:
     )
     topo_layer.add_to(m)
 
-#m.add_child(folium.LatLngPopup()) # enable this if you want to click to see the LatLng
+m.add_child(folium.LatLngPopup()) # enable this if you want to click to see the LatLng
 
 # routes / campsites
 
 KEY_SHORT_DESC = 'short_desc'
 KEY_LOCATION = 'location'
 KEY_WAYPOINTS = 'waypoints'
+
+point_adelaide_tarn = (-40.9430, 172.5435)
+desc_adelaide_tarn = 'Adelaide Tarn'
+point_trevor_carter = (-41.3983, 172.3886)
 
 route = [
     { KEY_SHORT_DESC: 'Road end', KEY_LOCATION: (-40.8098, 172.9563), },
@@ -51,10 +55,38 @@ route = [
     { KEY_SHORT_DESC: 'Wainui Hut', KEY_LOCATION: (-40.9147, 172.9203), },
     { KEY_WAYPOINTS: ((-40.9377, 172.9003), (-40.8970, 172.7300),),
       KEY_SHORT_DESC: 'Anatoki Forks Hut', KEY_LOCATION: (-40.9396, 172.6084), },
-    { KEY_SHORT_DESC: 'Adelaide Tarn', KEY_LOCATION: (-40.9430, 172.5435), },
+    { KEY_SHORT_DESC: desc_adelaide_tarn, KEY_LOCATION: point_adelaide_tarn, },
     { KEY_SHORT_DESC: 'Lonely Lake', KEY_LOCATION: (-40.9828, 172.5580), },
     { KEY_WAYPOINTS: ((-41.0357, 172.5494),),
       KEY_SHORT_DESC: 'Fenella', KEY_LOCATION: (-41.04960, 172.52520), },
+    { KEY_SHORT_DESC: 'Trilobite', KEY_LOCATION: (-41.1305, 172.6094), },
+    { KEY_WAYPOINTS: ((-41.1407, 172.6252), (-41.1474, 172.6044), (-41.1853, 172.6296), (-41.2153, 172.5904)),
+      KEY_SHORT_DESC: 'Karamea Bend', KEY_LOCATION: (-41.2329, 172.5109), },
+    { KEY_WAYPOINTS: ((-41.2933, 172.4685), (-41.3842, 172.4189), point_trevor_carter),
+      KEY_SHORT_DESC: 'Stone', KEY_LOCATION: (-41.4208, 172.4392), },
+    { KEY_WAYPOINTS: ((-41.4265, 172.4278), (-41.4538, 172.4242), (-41.4799, 172.3945), (-41.5032, 172.4002)),
+      # see https://www.doc.govt.nz/parks-and-recreation/places-to-go/nelson-tasman/places/kahurangi-national-park/things-to-do/tracks/matiri-valley-and-1000-acre-plateau-tramping-tracks/
+      KEY_SHORT_DESC: 'Hurricane', KEY_LOCATION: (-41.5068, 172.3822), },
+    { KEY_WAYPOINTS: ((-41.5872, 172.3631),),
+      KEY_SHORT_DESC: 'Lake Matiri', KEY_LOCATION: (-41.6565, 172.3291), },
+    { KEY_SHORT_DESC: 'Murchison', KEY_LOCATION: (-41.8069, 172.3247), },
+]
+
+alternate_routes = [
+  [
+    # Farewell Spit to Adelaide
+    { KEY_SHORT_DESC: 'Farewell Spit', KEY_LOCATION: (-40.5107, 172.7465), },
+    { KEY_WAYPOINTS: ((-40.5003, 172.6853), (-40.5146, 172.6471), (-40.5302, 172.6376), (-40.5540, 172.6308), (-40.5842, 172.6325), (-40.5965, 172.5803), (-40.6154, 172.5510), (-40.6397, 172.5596), (-40.6962, 172.5409), (-40.7194, 172.5806), (-40.7797, 172.5591), (-40.8227, 172.5791), (-40.8425, 172.5581), (-40.8798, 172.5832), (-40.9003, 172.5793), (-40.9191, 172.5534), (-40.9334, 172.5553), (-40.9393, 172.5498), (-40.9430, 172.5517)),
+      KEY_SHORT_DESC: desc_adelaide_tarn, KEY_LOCATION: point_adelaide_tarn, },
+    # tricky to connect Boulder Lake to Adelaide, but it is doable https://www.wildthings.club/trails/tasman/takaka/boulder-lake-to-anatoki-forks/?
+  ],
+  [
+    # Wangapeka to OGR
+    { KEY_SHORT_DESC: 'Trevor Carter Hut', KEY_LOCATION: point_trevor_carter, },
+    { KEY_WAYPOINTS: ((-41.3888, 172.3652), (-41.3972, 172.2952), (-41.3697, 172.2666), (-41.4229, 172.2426), (-41.4670, 172.2770), (-41.4664, 172.2388), (-41.4957, 172.2340), (-41.5147, 172.1969), (-41.5481, 172.1787), (-41.6056, 172.1994), (-41.6865, 172.2025), (-41.7234, 172.0984)),
+      # to connect Johnson Hut to OGR - http://remotehuts.co.nz/huts/Johnson_Hut/
+      KEY_SHORT_DESC: 'Old Ghost Road - Lyell', KEY_LOCATION: (-41.7959, 172.0497), },
+  ],
 ]
 
 # put it all together
@@ -118,6 +150,46 @@ for i, day in enumerate(route):
         weight=5,
         #popup='{}'.format(day[DAY_DATE]),
     ).add_to(fg_route)
+
+# Alternate camps
+fg_alternate_camps = folium.FeatureGroup(name='Alternate campsites', show=True)
+fg_alternate_camps.add_to(m)
+
+for r in alternate_routes:
+    for x in r:
+        popup = folium.Popup(x[KEY_SHORT_DESC])
+        marker = folium.Marker(
+            location=x[KEY_LOCATION],
+            popup=popup,
+        )
+        marker.add_to(fg_alternate_camps)
+
+# Alternate routes
+fg_alternate_routes = folium.FeatureGroup(name='Alternate routes', show=True)
+fg_alternate_routes.add_to(m)
+
+for r in alternate_routes:
+    for i, day in enumerate(r):
+        leg = []
+        if i == 0:
+            pass # do nothing!
+        else:
+            wake_up_coord = r[i-1][KEY_LOCATION]
+            leg.append(wake_up_coord)
+        if day.get(KEY_WAYPOINTS):
+            leg.extend(day[KEY_WAYPOINTS])
+        sleep_coord = day[KEY_LOCATION]
+        leg.append(sleep_coord)
+
+        if len(leg) < 2:
+            continue
+
+        folium.PolyLine(
+            locations=leg,
+            color='#8486d3',
+            weight=5,
+            #popup='{}'.format(day[DAY_DATE]),
+        ).add_to(fg_alternate_routes)
 
 # LayerControl - this has to come after all the FeatureGroups
 folium.LayerControl(collapsed=False).add_to(m)
